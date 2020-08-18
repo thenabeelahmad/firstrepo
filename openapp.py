a332d7a0c690322c3f86f7c60d6814bb23a1adee
Nabeel Ahmad <69769575+thenabeelahmad@users.noreply.github.com>
2020-08-19 00:41:17 +0530

    Create openapp.py
    Python code with user choice to open applications by command line.

---
+++ openapp.py
@@ -0,0 +1,19 @@
+import os
+
+print("Enter 1 to open notepad")
+print("Enter 2 to open windows media player")
+print("Enter 3 to open google chrome")
+
+inp=int(input("Enter final choice")
+
+if inp==1:
+  os.system('notepad')
+
+if inp==2:
+  os.system('wmplayer')
+
+if inp==3:
+  os.system('chrome')
+
+else:
+  print("can't handle your choice")
